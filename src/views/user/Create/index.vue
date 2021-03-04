2ee509f516d4093a74f5fbc5b1cdddc7010a94b7
BeRay31 <beray.prof.if@gmail.com>
2021-03-04 08:33:49 +0700

    template create user and list user

--- src/views/user/Create/index.vue
+++ src/views/user/Create/index.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="create-user-container">
-    <h1>Create User Page</h1>
+    <header>
+      <h1>Tambahkan Pengguna</h1>
+    </header>
+    <div class="content-contaienr">
+      <div class="form-card">
+        <el-form>
+          <el-form-item>
+            <MDInput
+              v-model="formData.user_id"
+              :maxlength="100"
+              required
+              name="Nomor Identitas"
+            >
+              Nomor Identitas
+            </MDInput>
+          </el-form-item>
+          <el-form-item>
+            <MDInput
+              v-model="formData.name"
+              :maxlength="100"
+              required
+              name="Nama Lengkap"
+            >
+              Nama Lengkap
+            </MDInput>
+          </el-form-item>
+          <el-form-item>
+            <MDInput
+              v-model="formData.username" 
+              :maxlength="100"
+              required
+              name="Username"
+            >
+              Username
+            </MDInput>
+          </el-form-item>
+          <el-form-item>
+            <MDInput
+              v-model="formData.password" 
+              :maxlength="100"
+              required
+              name="Password"
+              type="password"
+            >
+              Password
+            </MDInput>
+          </el-form-item>
+          <el-form-item label="Tipe User">
+            <el-radio-group v-model="formData.user_type">
+              <el-radio label="admin">Admin</el-radio>
+              <el-radio label="lecturer">Lecturer</el-radio>
+              <el-radio label="student">Student</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
+        <el-button
+          :loading="loading"
+          type="primary"
+          class="btn btn-primary btn-login"
+          @click.prevent="handleSubmit"
+        >Tambahkan</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import MDInput from '@/components/MDinput'
+
 export default {
   name: 'CreateUser',
   components: {
-
+    MDInput
   },
   data() {
     return {
-
+      formData: {
+        name: null,
+        username: null,
+        user_id: null,
+        email: null,
+        password: null,
+        user_type: null 
+      }
     }
   },
   methods: {
--- src/views/user/List/index.vue
+++ src/views/user/List/index.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="user-list-container">
-    <h1>User List Page</h1>
+    <header>
+      <h1>Daftar Pengguna</h1>
+    </header>
+    <div class="content-container">
+      <div class="card">
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Nomor Identitas</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Aksi</th>
+          </tr>
+          <tr>
+            <td>asd</td>
+            <td>asd</td>
+            <td>asd</td>
+            <td>asd</td>
+            <td class="action">
+              <el-button type="primary" icon="el-icon-edit">Edit</el-button>
+              <el-button type="primary" icon="el-icon-delete">Delete</el-button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'UserList',
-  components: {
-
-  },
   data() {
     return {
-
+      userData: []
     }
   },
   methods: {
