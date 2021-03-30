e102829a50719fbe55869ed4a2c3c12c27f56347
BeRay31 <beray.prof.if@gmail.com>
2021-03-30 15:44:56 +0700

    fixing err

--- src/views/courses/AddLoWeight/SubmitModal/index.vue
+++ src/views/courses/AddLoWeight/SubmitModal/index.vue
@@ -2,11 +2,7 @@
   <Modal :state="state" :title="title" @closeModal="$emit('closeModal')">
     <div class="content">
       <div class="content__body">
-<<<<<<< HEAD:src/views/courses/Modal/SubmitModal/index.vue
-        {{ content }}
-=======
         Komponen Learning Outcome akan ditambahkan pada mata kuliah
->>>>>>> 0bcaefdb98facbc1773156c7ffbf702805f18913:src/views/courses/AddLoWeight/SubmitModal/index.vue
       </div>
       <div class="content__button-group">
         <button class="btn btn-primary-alt" @click="$emit('closeModal')">Batalkan</button>
@@ -38,15 +34,7 @@ export default {
     },
     title: {
       type: String,
-<<<<<<< HEAD:src/views/courses/Modal/SubmitModal/index.vue
-      default: 'Buat Mata Kuliah?'
-    },
-    content: {
-      type: String,
-      default: 'Mata kuliah akan didaftarkan ke dalam sistem akreditasi.'
-=======
       default: 'Tambahkan Komponen?'
->>>>>>> 0bcaefdb98facbc1773156c7ffbf702805f18913:src/views/courses/AddLoWeight/SubmitModal/index.vue
     }
   }
 }
